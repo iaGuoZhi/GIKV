425d178e1b7d0aae75c2d2f6fd4ae1272270d86f
Guozhi-explore <2151259337@qq.com>
2020-06-08 23:22:48 +0800

    implement basic zk basic master and not bad worker

---
+++ src/zkservice/comment.go
@@ -0,0 +1,10 @@
+package zkservice
+
+const MasterPath = string("/GIKV/Master/Master")
+const RootPath = string("/GIKV")
+const WorkerPrimayPath = string("/GIKV/Worker/Primary")
+const WorkerViewServerPath = string("/GIKV/Worker/ViewServer")
+const MasterSlavePath = string("/GIKV/Master/Slave")
+const WorkerBackupPath = string("/GIKV/Worker/Backup")
+
+const ZkServer = string("127.0.0.1:2181")
